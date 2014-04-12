f01704c70ecad6dfcb2cbf099dbdccf382a73f28
Stefan Mark <mark@unserver.de>
2014-04-12 11:43:40 +0200

    more example scripts

--- examples/data/uzbl/scripts/download.sh
+++ examples/data/uzbl/scripts/download.sh
@@ -1,22 +1,50 @@
 #!/bin/sh
-# just an example of how you could handle your downloads
-# try some pattern matching on the uri to determine what we should do
+# extended download script
+# uses mimetype and mime list to determine what to do with downloads
+
+do=`echo -e "download\nabbrechen\nopen"|dmenu`
+if test "$do" != "abbrechen";
+then
 
 # Some sites block the default wget --user-agent..
-GET="wget --user-agent=Firefox"
+GET='wget --content-disposition --user-agent=Firefox -c -nv'
+# Get the Filename (with WGET, extract from stderr)
+GET_RETURN_FN=' 2>&1 | sed "s/^.* -> \"//g; s/\".*//g"'
+# 4 torrents
+TGET='ctorrent -D 100 -U 10 -e 1 -S '`hostname`':2780'
 
 dest="$HOME"
 url="$8"
-
 http_proxy="$9"
 export http_proxy
 
-test "x$url" = "x" && { echo "you must supply a url! ($url)"; exit 1; }
+test "x$url" = "x" && { notify-send "uzbl-dl" "you must supply a url! ($url)"; exit 1; }
 
-# only changes the dir for the $get sub process
-if echo "$url" | grep -E '.*\.torrent' >/dev/null;
+shift
+if test "$do" = "open";
 then
-    ( cd "$dest"; eval "$GET" "$url")
+  (
+    cd "$dest"
+    file=`eval "$GET" "\"$url\"" "$GET_RETURN_FN"`
+    if test "x$file" != "x";
+    then
+      app=`grep \`file -izb "$file" | sed 's/;.*//g'\` $HOME/.mimeapps | cut -f 2`
+      if test "x$app" != "x";
+      then
+        eval "$app" "\"$dest/$file\"" &
+      else
+	notify-send "Download finished:" "$url"
+      fi
+    fi
+  )
 else
-    ( cd "$dest"; eval "$GET" "$url")
+  if echo "$url" | grep -E '.*\.torrent' >/dev/null;
+  then
+      ( cd "$dest"; eval "$TGET" "\"$url\"")
+  else
+      ( cd "$dest"; eval "$GET" "\"$url\""; )
+  fi
+  notify-send "Download finished:" "$url"
+fi
+
 fi
--- examples/data/uzbl/scripts/load_url_from_bookmarks.sh
+++ examples/data/uzbl/scripts/load_url_from_bookmarks.sh
@@ -4,17 +4,27 @@
 
 file=${XDG_DATA_HOME:-$HOME/.local/share}/uzbl/bookmarks
 [ -r "$file" ] || exit
-COLORS=" -nb #303030 -nf khaki -sb #CCFFAA -sf #303030"
-if dmenu --help 2>&1 | grep -q '\[-rs\] \[-ni\] \[-nl\] \[-xs\]'
-then
-	DMENU="dmenu -i -xs -rs -l 10" # vertical patch
-	# show tags as well
-	goto=`$DMENU $COLORS < $file | awk '{print $1}'`
+
+if [ -z "$DMENUSETTINGS" ]; then
+    COLORS=" -nb #303030 -nf khaki -sb #CCFFAA -sf #303030"
 else
-	DMENU="dmenu -i"
-	# because they are all after each other, just show the url, not their tags.
-	goto=`awk '{print $1}' $file | $DMENU $COLORS`
+    COLORS=$DMENUSETTINGS
 fi
 
-#[ -n "$goto" ] && echo "uri $goto" > $4
-[ -n "$goto" ] && echo "uri $goto" | socat - unix-connect:$5
+#if dmenu --help 2>&1 | grep -q '\[-rs\] \[-ni\] \[-nl\] \[-xs\]'
+#then
+	DMENU="dmenu -i -l 10" # vertical patch
+	# show tags as well
+	goto=`$DMENU $COLORS < $file | awk '{print $1}'`
+#else
+#	DMENU="dmenu -i"
+#	# because they are all after each other, just show the url, not their tags.
+#	goto=`awk '{print $1}' $file | $DMENU $COLORS`
+#fi
+
+if [ "$8" = "new" ]
+then
+    [ -n "$goto" ] && echo "chain 'event SET_KEYCMD @OpenInNewWindowKey' 'event APPEND_KEYCMD $goto' 'event KEYCMD_EXEC_CURRENT' 'event SET_KEYCMD'" | socat - unix-connect:$5
+else
+    [ -n "$goto" ] && echo "uri $goto" | socat - unix-connect:$5
+fi
